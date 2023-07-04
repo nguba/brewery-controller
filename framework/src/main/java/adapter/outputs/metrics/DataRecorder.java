cb03cf6ea5d88eb3679b0ba44f7846674819b46f
Nico Guba <nico.guba@gmail.com>
2023-07-04 22:27:00 +0100

    documented intention of DataRecorder

--- framework/src/main/java/adapter/outputs/metrics/DataRecorder.java
+++ framework/src/main/java/adapter/outputs/metrics/DataRecorder.java
@@ -1,4 +1,9 @@
 package adapter.outputs.metrics;
 
+/**
+ * The DataRecorder is responsible for recording the metrics of the PXU.
+ * These are important for graphing the temperature profiles of each step in the brewing process.
+ * @see adapter.outputs.pxu.PxuMetrics
+ */
 public class DataRecorder {
 }
