b1289834c57d21a41a353230c69224576bda2004
Nico Guba <nico.guba@gmail.com>
2023-07-04 14:26:01 +0100

    discovery test on how to instantiate the influxdb client.

--- framework/src/test/java/adapter/outputs/metrics/DataRecorderTest.java
+++ framework/src/test/java/adapter/outputs/metrics/DataRecorderTest.java
@@ -1,8 +1,13 @@
 package adapter.outputs.metrics;
 
+import com.influxdb.client.InfluxDBClient;
+import com.influxdb.client.InfluxDBClientFactory;
+import com.influxdb.client.InfluxDBClientOptions;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.InfluxDBContainer;
 import org.testcontainers.utility.DockerImageName;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class DataRecorderTest {
 
@@ -11,9 +16,26 @@ class DataRecorderTest {
      */
     @Test
     void testContainerStartup() {
-        InfluxDBContainer<?> container = new InfluxDBContainer<>(
+        try(InfluxDBContainer<?> container = new InfluxDBContainer<>(
                 DockerImageName.parse("influxdb:2.7.1")
-        );
-        container.start();
+        );) {
+            container.start();
+
+            assertThat(container.isRunning()).isTrue();
+
+            InfluxDBClientOptions options = InfluxDBClientOptions.builder()
+                    .url(container.getUrl())
+                    //.authenticateToken(container.getToken())
+                    .org(container.getOrganization())
+                    .bucket(container.getBucket())
+                    .build();
+
+            try (InfluxDBClient client = InfluxDBClientFactory.create(options)) {
+
+                assertThat(client.ping()).isTrue();
+
+            }
+        }
+
     }
 }
