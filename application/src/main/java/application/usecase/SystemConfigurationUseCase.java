707770a66092f487a572c2ada5b7367558598092
Nico Guba <nico.guba@gmail.com>
2023-08-02 20:04:09 +0100

    added comment explaining use case.

--- application/src/main/java/application/usecase/SystemConfigurationUseCase.java
+++ application/src/main/java/application/usecase/SystemConfigurationUseCase.java
@@ -7,5 +7,11 @@ import domain.Vessel;
  */
 public interface SystemConfigurationUseCase {
 
+    /**
+     * Register a new vessel in the system.  Temperature controllers are mapped to the vessel.  This is necessary
+     * to enable other modules to locate the correct temperature controller for a given vessel on the modbus network.
+     *
+     * @param vessel
+     */
     void registerVessel(Vessel vessel);
 }
